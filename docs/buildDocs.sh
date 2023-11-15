a304772ee573e5651957acecfb301b387192d5ff
qbdl <1260017352@qq.com>
2023-11-15 16:28:58 +0800

    123

--- docs/buildDocs.sh
+++ docs/buildDocs.sh
@@ -14,7 +14,8 @@ set -x
 apt-get update
 # apt-get -y install git rsync python3-sphinx python3-sphinx-rtd-theme python3-pip
 apt-get -y install git rsync python3-pip
-pip3 install -r docs/requirements.txt
+# pip3 install -r docs/requirements.txt
+pip3 install --no-cache-dir -r docs/requirements.txt
 
 #####################
 # DECLARE VARIABLES #
